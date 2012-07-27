ac4622e30d2f150ebdb207139b144e3e313d8653
GennaroIvan <ivan__sa@hotmail.com>
2012-07-27 09:31:41 -0500

    Trabajé el login y el logout

---
+++ src/dao/SQL.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author Samsung
+ */
+public class SQL {
+    
+    public static String consultamotivos="SELECT MotivoEntrada FROM motivosentrada";
+    public static String nombreA="SELECT NombreAlumno where IdAlumno=?";
+    public static String nombreP="SELECT Nombre where IdEmpleado=?";
+    public static String nombreV="SELECT";
+    
+}
